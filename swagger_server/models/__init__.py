18bfaacb4ac8c25ccc0ff057bb01a409fed4cd31
@josiah-roberts <josiah.david.roberts@gmail.com>
2018-11-02 21:17:18 -0700

    Generation

--- swagger_server/models/__init__.py
+++ swagger_server/models/__init__.py
@@ -3,7 +3,4 @@
 # flake8: noqa
 from __future__ import absolute_import
 # import models into model package
-from swagger_server.models.conversion import Conversion
-from swagger_server.models.conversions import Conversions
-from swagger_server.models.error import Error
-from swagger_server.models.iso4217s import ISO4217s
+from swagger_server.models.latlong import Latlong
