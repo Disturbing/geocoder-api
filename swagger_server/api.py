09329f0e667658a75348425c9f3f0ad7353613fc
@josiah-roberts <josiah.david.roberts@gmail.com>
2018-11-03 15:13:57 -0700

    Pulled out unused function

--- swagger_server/api.py
+++ swagger_server/api.py
@@ -1,11 +1,6 @@
 from swagger_server.custom import CustomGoogleQuery
 from swagger_server import util
 
-def csv(list):
-    if list == None:
-        return None
-    return ','.join(map(str, list)) 
-
 class GeocoderApi(object):
     def get_locate(self, location=None, region=None, bounds=None, components=None):
         customArgs = {
