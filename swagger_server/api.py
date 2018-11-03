a026e301cecd8430237569110e273cc5d861569b
@josiah-roberts <josiah.david.roberts@gmail.com>
2018-11-03 14:30:57 -0700

    Implemented bounds support

--- swagger_server/api.py
+++ swagger_server/api.py
@@ -2,7 +2,12 @@ from swagger_server.custom import CustomGoogleQuery
 from swagger_server.models.latlong import Latlong  # noqa: E501
 from swagger_server import util
 
+def csv(list):
+    if list == None:
+        return None
+    return ','.join(map(str, list)) 
+
 class GeocoderApi(object):
     def get_locate(self, address=None, northeast=None, southwest=None):
-        g = CustomGoogleQuery(address, northeast=northeast, southwest=southwest)
+        g = CustomGoogleQuery(address, bounds=f"{csv(southwest)}|{csv(northeast)}")
         return Latlong(g.lat, g.lng)
