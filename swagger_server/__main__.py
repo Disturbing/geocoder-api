6b2112fe326155bcfb15662e0c55105a8148776d
@josiah-roberts <josiah.david.roberts@gmail.com>
2018-11-03 18:17:21 -0700

    Added GOOGLE_API_KEY check

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 import connexion
+import os
 
 from swagger_server import encoder
 
 
 def main():
+    if os.environ.get('KEY_THAT_MIGHT_EXIST') == None:
+        print("The environment variable \"GOOGLE_API_KEY\" is not set.\nPlease set your API key and try again")
+        exit()
+
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Geocoder'})
