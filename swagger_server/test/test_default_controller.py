9b6606d04d7e3cef26d21c3cabef6f86059cf2b9
ibeckermayer <ibeckermayer@gmail.com>
2018-11-03 17:43:55 -0700

    skeleton code for tests

--- swagger_server/test/test_default_controller.py
+++ swagger_server/test/test_default_controller.py
@@ -6,18 +6,79 @@ from flask import json
 from six import BytesIO
 
 from swagger_server.models.error import Error  # noqa: E501
+from swagger_server.test import BaseTestCase
 
 class TestDefaultController(BaseTestCase):
     """DefaultController integration test stubs"""
 
-    def test_get_forward(self):
+    def test_get_forward_location(self):
         """Test case for get_forward
+
+        Forward geocoding
+        """
+        query_string = [('location', 'santa cruz')]
+        response = self.client.open(
+            '/geocoder/forward',
+            method='GET',
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_forward_no_location(self):
+        """Test case for get_forward with no location (error)
 
         Forward geocoding
         """
         query_string = [('location', 'location_example'),
                         ('region', 'region_example'),
                         ('bounds', 'bounds_example'),
+                        ('components', 'components_example')]
+        response = self.client.open(
+            '/geocoder/forward',
+            method='GET',
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_forward_region(self):
+        """Test case for get_forward with region
+
+        Forward geocoding
+        """
+        query_string = [('location', 'location_example'),
+                        ('region', 'region_example'),
+                        ('bounds', 'bounds_example'),
+                        ('components', 'components_example')]
+        response = self.client.open(
+            '/geocoder/forward',
+            method='GET',
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_forward_bounds(self):
+        """Test case for get_forward with bounds
+
+        Forward geocoding
+        """
+        query_string = [('location', 'location_example'),
+                        ('region', 'region_example'),
+                        ('bounds', 'bounds_example'),
+                        ('components', 'components_example')]
+        response = self.client.open(
+            '/geocoder/forward',
+            method='GET',
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+    def test_get_forward_components(self):
+        """Test case for get_forward with components
+
+        Forward geocoding
+        """
+        query_string = [('location', 'location_example'),
                         ('components', 'components_example')]
         response = self.client.open(
             '/geocoder/forward',
